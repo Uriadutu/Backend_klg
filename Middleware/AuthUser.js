f8d0577d3273125f2e165e4fa2f9630fd8e8c265
Uriaaaa <126378661+Uriadutu@users.noreply.github.com>
2024-12-28 21:17:59 +0700

    Update AuthUser.js

--- Middleware/AuthUser.js
+++ Middleware/AuthUser.js
@@ -1,5 +1,5 @@
 import User from "../Models/UserModel.js";
-
+import User from "../Models/UserModel.js";
 export const verifyUser = async (req, res, next) =>{
     if(!req.session.userId){
         return res.status(401).json({msg: "Mohon login ke akun Anda!"});
